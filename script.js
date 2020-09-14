53b0582e1a0a06aed49f21966bfd7fc7dc01bd29
samir <samir@wu20.se>
2020-09-14 10:40:19 +0200

    added some button logic

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 //badly stored (public) secrets
-const username = "test";
-const password = "1234";
+const secretUsername = "test";
+const secretPassword = "1234";
 
-const username = document.getElementById(username);
-const username = document.getElementById(password);
-const loginButton = document.getElementById(loginButton);
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const loginButton = document.getElementById("loginButton");
 
+loginButton.onclick = function() {
+	const user = username.value;
+	const pass = password.value;
+
+	if (user === secretUsername &&
+		pass === secretPassword) {
+		
+		console.log("Välkommen!");
+		//use DOM to render: welcome message, log out button
+		//use localStorage to persist successful login
+		localStorage.setItem(user, pass);
+
+		// localStorage.setItem(user, pass);
+		// location.reload();
+	} else {
+		console.log("Något gick fel!");
+		//use DOM to render: bad login message, login screen
+		//remove login item from localStorage
+		// localStorage.removeItem(user, pass);
+	}
+};
