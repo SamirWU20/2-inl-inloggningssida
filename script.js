30c619f95fddbe4f0308166d315fbd412195d050
samir <samir@wu20.se>
2020-09-14 09:56:21 +0200

    added input fields

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+//badly stored (public) secrets
 const username = "test";
 const password = "1234";
+
+const username = document.getElementById(username);
+const username = document.getElementById(password);
+const loginButton = document.getElementById(loginButton);
+
